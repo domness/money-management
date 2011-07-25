8ca5138b4df3ba9cf55d50b6780911db531abde1
Dominic Wroblewski <domness@gmail.com>
2011-07-25 23:25:57 +0100

    When creating a new user bill, the date is now included with the bill name

--- app/models/bill.rb
+++ app/models/bill.rb
@@ -3,4 +3,8 @@ class Bill < ActiveRecord::Base
   has_many :user_bills, :dependent => :destroy
   has_many :users, :through => :user_bills
   
+  def to_label
+    "#{due.to_s} #{name}"
+  end
+  
 end
